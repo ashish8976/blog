cca83255724347a97b9c7041316e589703f4d6e6
Ashish Rajput <ashish8976>
2026-06-06 02:41:03 +0530

    Fix: hide credentials using decouple

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v(sk4v0gbh9!@6#m4=)x&2)$qjw&lmhq1(td24*+ojtfb(-cyu"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +80,7 @@ DATABASES = {
         "NAME": "blog_database",
         "USER":"postgres",
         "HOST":"localhost",
-        "PASSWORD":"12345",
+        "PASSWORD": config('DB_PASSWORD'),
         "PORT":"5432"
     }
 }
@@ -126,7 +127,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ashishrajput8156@gmail.com'
-EMAIL_HOST_PASSWORD = 'cyqi jjnj jnkm kskm'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 AUTH_USER_MODEL = 'writesphere.User' 
 
